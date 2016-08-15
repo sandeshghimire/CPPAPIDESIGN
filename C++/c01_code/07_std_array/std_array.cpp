f6464d24426f62ae2e2fac55dce7764b9819af48
Sandesh J. Ghimire <sandesh@vavehealth.com>
2016-08-15 13:13:46 -0700

    Exception

--- C++/c01_code/07_std_array/std_array.cpp
+++ C++/c01_code/07_std_array/std_array.cpp
@@ -15,6 +15,8 @@ int main()
     }
 
 
+
+
     std::array<int, 3> arr = {9, 8, 7};
     std::cout << "Array size = " << arr.size() << std::endl;
     std::cout << "Element 2 = " << arr[1] << std::endl;
--- C++/c01_code/12_ConstArg/ConstArg.cpp
+++ C++/c01_code/12_ConstArg/ConstArg.cpp
@@ -1,18 +1,69 @@
 #include <string>
+#include <iostream>
 
-
-namespace D1
+namespace DX
 {
-    void mysteryFunction(const std::string *someString)
+    void mysteryFunction(std::string *someString)
     {
         *someString = "Test";  // Will not compile.
     }
+
+
+    void mysteryFunction2(const int input)
+    {
+        std::cout << "Input is " << input << std::endl;
+    }
+
+    void mysteryFunction3(const std::string &input)
+    {
+        std::cout << "Input is " << input << std::endl;
+    }
+
+    void mysteryFunction4(const int *const input)
+    {
+
+        std::cout << "Input is " << *input << std::endl;
+    }
+
+    void mysteryFunction5(const int * input)
+    {
+
+        std::cout << "Input is " << *input << std::endl;
+    }
+
+
+    void mysteryFunction6(int *const input)
+    {
+
+        std::cout << "Input is " << *input << std::endl;
+    }
+
+
+    void mysteryFunction7(int * input)
+    {
+
+        std::cout << "Input is " << *input << std::endl;
+    }
+
+
 }
 
 int main()
 {
+    int * input = (int *) 34;
     std::string myString = "The string";
-    mysteryFunction(&myString);
+    DX::mysteryFunction(&myString);
+    DX::mysteryFunction3(myString);
+
+    DX::mysteryFunction4(input);
+    DX::mysteryFunction5(input);
+    DX::mysteryFunction6(input);
+    DX::mysteryFunction7(input);
+
+
+
+
+
     return 0;
 }
 
