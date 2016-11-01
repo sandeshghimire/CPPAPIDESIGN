15747b8cc954c78be4079a921ca48333b92d37f4
Sandesh Ghimire <sandeshghimire@outlook.com>
2016-11-01 15:06:47 -0700

    Update

--- CESS/example18.c
+++ CESS/example18.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 
+int reverse(char * msg);
+
 
 int main(int argc, char** argv)
 {
-    printf("Hello World");
-    return 0;
+    char * msg = "the house and the mouse and the cat and the hat and mike and trick";
+    printf("%s \n", msg);
+
+    printf("the length is %d \n ",reverse(msg));
+
+    printf("The string is %s \n", msg);
+
+}
+
+int reverse(char * msg)
+{
+    int len = 0;
+    char * ch = msg;
+    int i = 0;
+
+    while(*ch++ != '\0')
+    {
+        len++;
+    }
+
+    for(i = 0; i < len/2; i++)
+    {
+        ch = msg[i];
+        printf("ch is %c \n", ch);
+        *(msg+i) = *(msg - len -i);
+        //msg[len-i] = ch;
+    }
+
+
+    return len;
 }
--- CESS/example19.c
+++ CESS/example19.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+unsigned long long  fact(unsigned int n);
 
-int main(int argc, char** argv)
-{
-    printf("Hello World");
-    return 0;
+int main(int argc, char **argv) {
+  unsigned int n = 10;
+  printf("Factorial of %X is %X \n", n, fact(n));
+}
+
+unsigned long long  fact(unsigned int n) {
+  if (n <= 1) {
+    return 1;
+  } else {
+    return (n * fact(n - 1));
+  }
 }
