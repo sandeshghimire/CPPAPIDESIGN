7360745bf9d90085ad18cb9ae6d23aeca67826d6
Sandesh Ghimire <sandeshghimire@gmail.com>
2016-11-03 22:07:08 -0700

    Update

---
+++ CPPESS/example.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main(int argc , char ** argv)
+{
+    return 0;
+
+}
+
+
+/*
+static void reverse(struct node** head_ref)
+{
+    struct node* prev   = NULL;
+    struct node* current = *head_ref;
+    struct node* next;
+    while (current != NULL)
+    {
+        next  = current->next;  
+        current->next = prev;   
+        prev = current;
+        current = next;
+    }
+    *head_ref = prev;
+}*/
+
+
+
+
+
