69f643859517b5d6263517e31e19d864b2a61513
Sandesh Ghimire <sandeshghimire@gmail.com>
2016-11-11 10:25:47 -0800

    Adding Sample and Twitter

--- CESS/example29.c
+++ CESS/example29.c
@@ -3,17 +3,7 @@
 
 int main(int argc, char** argv)
 {
-    unsigned int number = 0x01;
-
-    // shift bit by 9
     
-    printf("the number is %d \n", number);
-
-    // shift bit by 9
-
-    number |= number << 9;
-
-    printf("the number is %d \n", number);
 
     return 0;
 }
